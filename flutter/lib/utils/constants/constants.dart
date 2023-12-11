55a2325e98ea6cd32180c349bc50981e6fac787b
senateseo <swdanielseo@gmail.com>
2023-12-10 17:27:42 -0800

    api url setup

--- flutter/lib/utils/constants/constants.dart
+++ flutter/lib/utils/constants/constants.dart
@@ -60,7 +60,7 @@ const settingsThemeKey = "theme";
 //Add your database url
 //make sure to not add '/' at the end of url
 // make sure to check if admin panel is http or https
-const databaseUrl = "https://elitequiz.wrteam.me";
+const databaseUrl = "http://gptplatform.org";
 
 const baseUrl = '$databaseUrl/Api/';
 
